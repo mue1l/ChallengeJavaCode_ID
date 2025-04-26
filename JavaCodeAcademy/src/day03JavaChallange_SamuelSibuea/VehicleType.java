34a53872b015239a961c1e298b3316e78f3318f2
mue1l <103925417+mue1l@users.noreply.github.com>
2025-04-27 02:52:31 +0700

    Delete JavaCodeAcademy directory

--- JavaCodeAcademy/src/day03JavaChallange_SamuelSibuea/VehicleType.java
+++
@@ -1,9 +0,0 @@
-package day03JavaChallange_SamuelSibuea;
-
-public enum VehicleType {
-    SUV,
-    Taxi,
-    PrivateJet,
-    Boat,
-    Vehicle
-}
--- JavaCodeAcademy/src/day03_Samuel_ChallangeJava/Main.java
+++
@@ -1,7 +0,0 @@
-package day03_Samuel_ChallangeJava;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
